ac0d10575e0d7d36ac32c33cb6478fc5fca921b8
Kestutis Januskevicius <infohata@gmail.com>
2023-02-22 10:08:31 +0200

    BookInstanceAdmin

--- ptu8_library/library/admin.py
+++ ptu8_library/library/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from . import models
 
 class BookAdmin(admin.ModelAdmin):
@@ -6,7 +7,16 @@ class BookAdmin(admin.ModelAdmin):
     list_display_links = ('title', )
 
 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (_('General'), {'fields': ('id', 'book')}),
+        (_('Availability'), {'fields': (('status', 'due_back'),)}),
+    )
+
+
 admin.site.register(models.Genre)
 admin.site.register(models.Author)
 admin.site.register(models.Book, BookAdmin)
-admin.site.register(models.BookInstance)
+admin.site.register(models.BookInstance, BookInstanceAdmin)
