f79d2619ec7f06b2e843bfd2c3368c49c82d5e48
Kestutis Januskevicius <infohata@gmail.com>
2023-02-22 09:49:10 +0200

    BookAdmin

--- ptu8_library/library/admin.py
+++ ptu8_library/library/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from . import models
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'display_genre')
+    list_display_links = ('title', )
+
 
 admin.site.register(models.Genre)
 admin.site.register(models.Author)
-admin.site.register(models.Book)
+admin.site.register(models.Book, BookAdmin)
 admin.site.register(models.BookInstance)
--- ptu8_library/library/models.py
+++ ptu8_library/library/models.py
@@ -42,6 +42,10 @@ class Book(models.Model):
     class Meta:
         ordering = ['title']
 
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all())
+    display_genre.short_description = _('genre(s)')
+
 
 class BookInstance(models.Model):
     id = models.UUIDField(
@@ -56,7 +60,7 @@ class BookInstance(models.Model):
         related_name='book_instances',
         verbose_name=_('book'),
     )
-    due_back = models.DateField(_('due_back'), null=True, blank=True, db_index=True)
+    due_back = models.DateField(_('due back'), null=True, blank=True, db_index=True)
 
     LOAN_STATUS = (
         ('m', _('managed')),
