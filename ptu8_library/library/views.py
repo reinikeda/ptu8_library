d4345c7dadd1a737509eb8ad08c2d4512c175e57
Kestutis Januskevicius <infohata@gmail.com>
2023-02-22 15:42:29 +0200

    index view and template; base template; base css initial version;

--- ptu8_library/library/views.py
+++ ptu8_library/library/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . import models
 
 
 def index(request):
-    return HttpResponse('Sveiki Studentai!')
+    book_count = models.Book.objects.count()
+    book_instance_count = models.BookInstance.objects.count()
+    available_bi_count = models.BookInstance.objects.filter(status='a').count()
+    author_count = models.Author.objects.count()
+    return render(request, 'library/index.html', {
+        'book_count': book_count,
+        'book_instance_count': book_instance_count,
+        'available_bi_count': available_bi_count,
+        'author_count': author_count,
+    })
