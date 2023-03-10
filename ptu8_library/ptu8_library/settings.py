827df77b494e245560e24369bef920ccb56bf785
Kestutis Januskevicius <infohata@gmail.com>
2023-03-10 08:40:17 +0200

    added postgres to docker composition; project config with postgres

--- ptu8_library/ptu8_library/settings.py
+++ ptu8_library/ptu8_library/settings.py
@@ -84,13 +84,23 @@ WSGI_APPLICATION = 'ptu8_library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "db",
+        "NAME": "ptu8_library",
+        "USER": "ptu8_library",
+        "PASSWORD": "nesakysiu",
+        "PORT": 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
